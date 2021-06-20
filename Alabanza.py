94bc1d074a75cccc3b1f8e9a55f9fb9407aeb0f6
daniel bobadilla <danybavila@gmail.com>
2021-06-20 01:06:31 -0500

    Segundo commit iniciamos a probar con archivos

--- Alabanza.py
+++ Alabanza.py
@@ -1,7 +1,10 @@
+from os import close
 import random
-# file = open("base_de_datos.txt")
+
 drummers = ["Daniel","Alejandro","yeny","Samuel"]
 number_drummers = random.randint(0, 3)
+
+
 
 # confra = int(input("Hay culto de confraternidad?.. Si si 1, si no 0: "))
 confra = 0
@@ -18,13 +21,20 @@ while i < 4:
     number_drummers = random.randint(0, 3)
     drummer = drummers[number_drummers]
     if not(i == 2 and drummer == "Samuel"):
-        if drummer in salida:
-            pass
-        else:
+        if not(drummer in salida):
             salida.append(drummer)
             i += 1
+file = open("base_de_datos.txt",'r')
+contents = file.read()
+i = 0
+while i < 4:
+    if contents[i] == salida[i]:
+        print("ERROR")
+    i += 1
+file.close()
 
-    
-
-
+file = open("base_de_datos.txt",'a')
+file.write("\n")
+file.write(str(salida))
+file.close()
 print(salida)
